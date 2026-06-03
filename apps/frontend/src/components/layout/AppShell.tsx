a8cabe9fc2dbe43936e31a1c06bfd9300653faf4
mvinduwara <mvinduwara@gmail.com>
2026-06-03 15:49:10 +0530

    Update AppShell.tsx

--- apps/frontend/src/components/layout/AppShell.tsx
+++ apps/frontend/src/components/layout/AppShell.tsx
@@ -2,9 +2,12 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 import { useUIStore } from "../../store/uiStore";
+import { useRealtimeEvents } from "../../hooks/useRealtimeEvents";
 
 export default function AppShell() {
   const closeNotifPanel = useUIStore((s) => s.closeNotifPanel);
+
+  useRealtimeEvents();
 
   return (
     <div
@@ -12,7 +15,7 @@ export default function AppShell() {
       onClick={() => closeNotifPanel()}
     >
       <Sidebar />
-      <div className="flex flex-1 flex-col overflow-hidden">
+      <div className="flex flex-1 flex-col overflow-hidden min-w-0">
         <Topbar />
         <main className="flex-1 overflow-y-auto p-6">
           <Outlet />
