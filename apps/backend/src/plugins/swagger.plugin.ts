9443baa88bec89a11dc690101c99743d032b3628
mvinduwara <mvinduwara@gmail.com>
2026-06-03 15:48:34 +0530

    Create swagger.plugin.ts

---
+++ apps/backend/src/plugins/swagger.plugin.ts
@@ -0,0 +1,37 @@
+import fp from "fastify-plugin";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import type { FastifyPluginAsync } from "fastify";
+
+const swaggerPlugin: FastifyPluginAsync = async (fastify) => {
+  await fastify.register(fastifySwagger, {
+    openapi: {
+      openapi: "3.0.0",
+      info: {
+        title: "WarehouseOS API",
+        description: "Warehouse Management System REST API",
+        version: "1.0.0",
+      },
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: "http",
+            scheme: "bearer",
+            bearerFormat: "JWT",
+          },
+        },
+      },
+      security: [{ bearerAuth: [] }],
+    },
+  });
+
+  await fastify.register(fastifySwaggerUi, {
+    routePrefix: "/docs",
+    uiConfig: {
+      docExpansion: "list",
+      deepLinking: false,
+    },
+  });
+};
+
+export default fp(swaggerPlugin);
